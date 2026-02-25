5894ba186ddd40cf0daec55dc09825dab35fc738
Oti254 <otrated555@gmail.com>
2026-02-26 01:14:58 +0300

    implementing 60fps

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Getting new instance of GUI window
-    running = True
-    while True: # Creating the infinite loop for the game loop
+    clock = pygame.time.Clock()                  # Creating the clock object
+    dt = 0
+
+
+    while True:                                  # Creating the infinite loop for the game loop
         log_state()
-        for event in pygame.event.get(): # Processing pygame event queue
-            # Checks if the user closed the window and exit the game loop if they do
-            if event.type == pygame.QUIT:
+
+        for event in pygame.event.get():         # Processing pygame event queue
+            if event.type == pygame.QUIT:        # Checks if the user closed the window and exit the game loop if they do
                 return
-        screen.fill("black") # Filling the screen with a solid black color
-        pygame.display.flip() # Refreshing the screen
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")                    # Filling the screen with a solid black color
+        pygame.display.flip()                   # Refreshing the screen
 
+        
+       # Limiting the frame rate to 60fps
+       # Pauses the game loop until 1/60th of a second has passed and returns delta time(ms)
+        dt = clock.tick(60)/1000                # Delta time in seconds
 
 if __name__ == "__main__":
     main()
